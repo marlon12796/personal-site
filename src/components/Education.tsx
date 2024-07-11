68c6761d4efe8f55d07fc5fcaa0974be6eb4003a
marlon ureta <fabrisio021@gmail.com>
2024-07-10 19:14:23 -0500

    added translation education

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,17 +1,20 @@
 import { EductionData } from '../app/data/content/education'
 import React from 'react'
 import { EducationCard } from './EducationCard'
+import { useTranslations } from 'next-intl'
 const Education = () => {
+  const t = useTranslations('about.eductionData')
+  const keys = ['zegelIpaeInstitute', 'secondaryEducation', 'primaryEducation'] as const
   return (
     <div className='[margin-block-start:1.5rem] relative after:content-[" "] after:w-[0.2rem] after:h-full after:absolute after:bg-slate-200 after:inset-0 sm:after:inset-[0_0_auto_50%]'>
-      {EductionData.map((education, index) => (
+      {keys.map((key, index) => (
         <EducationCard
-          title={education.title}
-          description={education.description}
-          key={education.title}
-          headerIcon={education.icon}
+          title={t(`${key}.title`)}
+          description={t(`${key}.description`)}
+          key={key}
+          headerIcon={EductionData[key]}
           even={(index + 1) % 2 === 0}
-          time={education.date}
+          time={t(`${key}.date`)}
         />
       ))}
     </div>
